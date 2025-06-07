2e781f0d94ace71282d02cd027ab1dcf78191d72
nero31994 <arenaofvalorph937@gmail.com>
2025-06-07 08:12:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const API_KEY = '488eb36776275b8ae18600751059fb49';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const SERVERS = {
   movie: [
-      { name: 'MainServer', url: 'https://https://vidify.top//embed/movie/' },
+      { name: 'MainServer', url: 'https://vidify.top//embed/movie/' },
     { name: 'Server1', url: 'https://spencerdevs.xyz/movie/' },
     { name: 'Server2', url: 'https://autoembed.pro/embed/movie/' }
   
