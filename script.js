73ba3f1c46803125f17575368b49ab7a538e23ff
nero31994 <arenaofvalorph937@gmail.com>
2025-09-01 21:39:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const API_KEY = '488eb36776275b8ae18600751059fb49';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const SERVERS = {
   movie: [
-      { name: 'MainServer', url: 'https://autoembed.pro/embed/tv/' },
+      { name: 'MainServer', url: 'https://autoembed.pro/embed/movie/' },
  
   ],
   tv: [
